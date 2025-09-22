6020f8ab68eea35724be01b0dafdbe0bffb8ec80
La Cantina Berlin <admin@lacantina-berlin.com>
2025-09-22 20:31:11 +0200

    Add client/src/components/examples/ReservationForm.tsx

---
+++ client/src/components/examples/ReservationForm.tsx
@@ -0,0 +1,9 @@
+import ReservationForm from '../ReservationForm'
+
+export default function ReservationFormExample() {
+  return (
+    <div className="p-8 bg-card">
+      <ReservationForm />
+    </div>
+  )
+}
